dc0a2e7a4fa0b443a34896f5f8bee8f0c245e628
BryanJackson1 <bryanandaimee@byui.edu>
2022-05-24 21:21:48 -0700

    adding content to main page.

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,5 +1,5 @@
 const currentDate = new Date();
-
+const day = currentDate.getDay();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
 document.querySelector('#date').textContent = fulldateUK;
 
@@ -9,3 +9,8 @@ const navbutton = document.querySelector('.navbtn');
 const mainnav = document.querySelector('.main-navigation');
 
 navbutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+const invite = document.querySelector('#invite');
+if (day === 1 || day === 2) {
+    invite.style.display = "block";
+}
