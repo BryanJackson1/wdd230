8bd3a7f990e26dcdf3525cdef7d890844c27bf8b
BryanJackson1 <bryanandaimee@byui.edu>
2022-07-02 19:44:26 -0700

    Weather API

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,13 +1,53 @@
-var chill = "init";
-const wind = parseFloat(document.getElementById('speed').textContent);
-const temp = parseFloat(document.getElementById('temp').textContent);
+var t = "init";
+var s = "init";
+var wc = "init";
+// select HTML elements in the document
+const temp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const wind = document.querySelector('#speed');
+const chill = document.querySelector('#chill');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Mount Vernon,WA,US&appid=035d9944159398699574a1990d3151c9&units=imperial';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  function  displayResults(weatherData) {
+    temp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    wind.innerHTML = `<strong>${weatherData.wind.speed.toFixed(1)}</strong>`;
+
+    t = parseFloat(weatherData.main.temp);
+    s = parseFloat(weatherData.wind.speed);
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+    
+
+    if ((s > 3) && (t < 50)) {
+        wc = 35.740 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
+        chill.textContent = wc.toFixed(1);
+    } else {
+        chill.textContent = "NA";
+    }
+  }
+  apiFetch();
 
 
-if ((wind > 3) && (temp < 50)) {
-    chill = 35.740 + 0.6215 * temp - 35.75 * wind ** 0.16 + 0.4275 * temp * wind ** 0.16;
-}
-else {
-    chill = "NA";
-}
 
-document.querySelector('#chill').textContent = chill.toFixed(1);
+
